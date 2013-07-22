01535682dbebd40fa1dbda279bed4614f541de71
Eric Liao <rcliao01@gmail.com>
2013-07-22 03:14:52 -0700

    udpate the db to support new table, still need to update the server side call

--- game_db.py
+++ game_db.py
@@ -33,26 +33,27 @@ class GameDB():
 			cur.execute("create table \
 				Bots(\
 					id INTEGER PRIMARY KEY AUTOINCREMENT,\
+					FORIEGN KEY(owner_id) REFERENCES Users(id),\
 					name TEXT UNIQUE,\
-					language TEXT,\
-					owner_id TEXT\
+					language TEXT\
 				)")
 
 			#### Tournaments ####
 			cur.execute("create table \
 				Tournaments(\
 					id INTEGER PRIMARY KEY AUTOINCREMENT,\
+					FORIEGN KEY(creator_id) REFERENCES Users(id),\
 					name TEXT UNIQUE,\
-					owner_id TEXT,\
 					password TEXT,\
-					started DATE,\
-					ends DATE\
+					start_date DATE,\
+					end_date DATE\
 				)")
 
 			#### Gameindex ####
 			cur.execute("create table \
 				Tourn_GameIndex(\
 					id INTEGER PRIMARY KEY AUTOINCREMENT,\
+					FORIEGN KEY(tourn_id) REFERENCES Tournaments(id),\
 					player TEXT,\
 					gameid INTEGER\
 				)")
@@ -60,7 +61,8 @@ class GameDB():
 			#### Games ####
 			cur.execute("create table \
 				Tourn_Games(\
-					id INTEGER,\
+					id INTEGER primary key AUTOINCREMENT,\
+					FORIEGN KEY(tourn_id) REFERENCES Tournaments(id),\
 					players TEXT,\
 					map TEXT,\
 					datum DATE,\
@@ -72,8 +74,8 @@ class GameDB():
 			cur.execute("create table \
 				Tourn_Entries(\
 					id INTEGER PRIMARY KEY AUTOINCREMENT,\
-					name TEXT,\
-					tournament_name TEXT,\
+					FORIEGN KEY(tourn_id) REFERENCES Tournaments(id),\
+					FORIEGN KEY(bot_id) REFERENCES Bots(id),\
 					lastseen DATE,\
 					rank INTEGER DEFAULT 1000,\
 					skill real DEFAULT 0.0,\
@@ -86,7 +88,8 @@ class GameDB():
 			#### Replays ####
 			cur.execute("create table \
 				Tourn_Replays(\
-					id INTEGER, \
+					id INTEGER,\
+					FORIEGN KEY(tourn_id) REFERENCES Tournaments(id),\
 					json BLOB\
 				)")
 
@@ -101,7 +104,7 @@ class GameDB():
 
 	#### SQL INTERFACE ####
 			
-	def now(self):
+	def now( self ):
 		return datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S") #asctime()
 
 	def update_defered( self, sql, tup=() ):
@@ -119,126 +122,110 @@ class GameDB():
 
 	#### READ ####
 
-	def get_replay( self, i ):
-		rep = self.retrieve("select json from replays where id=?", (i,) )
+	def get_replay( self, t_id, id ):
+		rep = self.retrieve("select json from Tourn_Replays where tourn_id=? id=?", (t_id, id) )
 		return zlib.decompress(rep[0][0])
-		#~ return rep[0][0]
 
-	def num_games( self ):
-		return int(self.retrieve( "select count(*) from games" )[0][0])
+	def num_games( self, t_id ):
+		return int(self.retrieve( "select count(*) from Tourn_Games where tourn_id=?", (t_id, ) )[0][0])
 
-	def get_games( self, offset, num):
-		return self.retrieve( "select * from games order by id desc limit ? offset ?", (num,offset) )
+	def get_games( self, t_id, offset, num ):
+		return self.retrieve( "select * from Tourn_Games where tourn_id=? order by id desc limit ? offset ?", (t_id, num, offset) )
 
-	#DEPRECATED
-	def get_games_for_player( self, offset, num, player):
-		arr = self.retrieve( "select gameid from gameindex where player=? order by gameid desc limit ? offset ?", (player,num,offset) )
+	def get_games_for_player( self, t_id, offset, num, player):
+		arr = self.retrieve( "select gameid from Tourn_GameIndex where tourn_id=? player=? order by gameid desc limit ? offset ?",
+			(t_id, player, num, offset) )
 		g = []
 		for a in arr:
-			z = self.retrieve( "select * from games where id=?", (a[0],))
-			g.append( z[0]  )
+			z = self.retrieve( "select * from Tourn_Games where tourn_id=? id=?", (t_id, a[0]))
+			g.append( z[0] )
 		return g
+	
+	def num_games_for_player( self, t_id, player ):
+		return int(self.retrieve( "select count(*) from Tourn_GameIndex where tourn_id=? player=?",
+			( t_id, player ) )[0][0] )
 
-	#DEPRECATED		
-	def num_games_for_player( self, player ):
-		return int(self.retrieve( "select count(*) from gameindex where player=?",(player,) )[0][0])
+	def num_players( self, t_id ):
+		return int(self.retrieve( "select count(*) from Tourn_Entries where tourn_id=?", (t_id, ) )[0][0])
 
-	#DEPRECATED
-	def num_players( self ):
-		return int(self.retrieve( "select count(*) from players" )[0][0])
+	def get_bots( self, u_id ):
+		return self.retrieve("select * from Bots where owner_id=?", (u_id, ))
 
-	def get_bots(self, username):
-		return self.retrieve("select * from bots where owner = '%s';" % username)
-
-	def authenticate_user( self, name, password):
-		sql = "select id from users where name = '%s' and password = '%s';" % (name, password)
-		return self.retrieve(sql)
+	def authenticate_user( self, name, password ):
+		return self.retrieve("select id from Users where name=? and password=?", (name, password))
 
 	# Checks if a username is available
 	# Returns True if available
 	# Returns False if not available (already in database)
-	def check_username(self, username):
-		sql = "select id from users where name = '%s';" % username
-		if self.retrieve(sql):
+	def check_username( self, username ):
+		if self.retrieve("select id from Users where name=?", (username, )):
 			return False
 		else:
 			return True
 
-	def get_tournaments(self, tournamentname = ''):
+	def get_tournaments( self, tournamentname = '' ):
 		if tournamentname:
-			return self.retrieve("select * from tournaments where name = '%s';" % tournamentname)
+			return self.retrieve("select * from Tournaments where name=?", (tournamentname, ))
 		else:
-			return self.retrieve("select * from tournaments")
+			return self.retrieve("select * from Tournaments")
 
-	def get_bot_tournaments(self, botname):
-		return self.retrieve("select * from bots_tournament where name = '%s';" % botname)
+	def get_bot_tournaments( self, t_id, bot_id ):
+		return self.retrieve("select * from Tourn_Entries where tourn_id=? bot_id=?", (t_id, bot_id))
 
-	def get_kill_client(self):
+	def get_kill_client( self ):
 		sql = "select * from kill_client"
 		return self.retrieve(sql)
 
-	#DEPRECATED
-	def get_player_lastseen(self, name):
-		return self.retrieve("select lastseen from players where name = '%s';" % name)
+	def get_player_lastseen( self, t_id, bot_id ):
+		return self.retrieve("select lastseen from Tourn_Entries where tourn_id=? bot_id=?", (t_id, name))
 
-	#DEPRECATED
-	def get_player( self, names ):
-		sql = "select * from players where name=?"
-		for n in names[1:]:
-			sql += " or name=?" 
-		return self.retrieve(sql, names )
+	def get_player( self, t_id, bot_id ):
+		sql = "select * from Tourn_Entries where tourn_id=? bot_id=?"
+		return self.retrieve( sql, (t_id, bot_id) )
 
 	#### WRITE ####
 
-	def add_replay( self, i, txt ):
+	def add_replay( self, t_id, id, txt ):
 		#~ data = txt
 		data = buffer(zlib.compress(txt))
-		self.update("insert into replays values(?,?)", (i,data) )
+		self.update("insert into Tourn_Replays values(?,?)", (id, t_id, data) )
 		
-	def add_game( self, i, map, turns, draws, players ):
-		self.update("insert into games values(?,?,?,?,?,?)", (i,players,self.now(),map,turns,draws))
+	def add_game( self, t_id, i, map, turns, draws, players ):
+		self.update("insert into Tourn_Games values(?,?,?,?,?,?)", (i, t_id, players, self.now(), map, turns, draws))
 		
-	def add_tournament(self, username, tournamentname, password =''):
+	def add_tournament( self, t_id, username, tournamentname, password =''):
 		#TODO change ends date
-		self.update("insert into tournaments values(?,?,?,?,?,?)", (None, tournamentname, username, password, self.now(), self.now()))
+		self.update("insert into Tournaments values(?,?,?,?,?,?)", (None, tournamentname, username, password, self.now(), self.now()))
 	
-	def add_user( self, name, password, firstname, lastname ):
-		self.update("insert into users values(?,?,?,?,?)", (None, name, password, firstname, lastname))
+	def add_user( self, name, password, email ):
+		self.update("insert into Users values(?,?,?,?,?)", (None, name, password, email))
 
-	#DEPRECATED
-	def add_player( self, name,password, language='unspecified' ):
-		#old
-		self.update("insert into players values(?,?,?,?,?,?,?,?,?,?)", (None,name,password,self.now(),1000,0.0,50.0,50.0/3.0,0,1))
-		#new
-		self.update("insert into bots values(?,?,?,?)", (None, name, password, language))
+	def enroll_bot( self, u_id, bot_id, t_id ):
+		self.update("insert into Tourn_Entries values(?,?,?,?)",
+			(None, t_id, b_id, self.now()))
 
-	def add_bot( self, username, botname, language, password = 'password'):
-		self.update("insert into bots values(?,?,?,?)", (None, name, password, language))
+	def add_bot( self, u_id, botname, language ):
+		self.update("insert into Bots values(?,?,?,?)", (None, u_id, botname, language))
 
-	def enroll_bot(self, username, botname, tournamentname):
-		self.update("insert into bots_tournament values(?,?,?,?,?,?,?,?,?)", (None, name, tournamentname, self.now(), 1000, 0.0, 50.0, 50.0/3.0, 0, 1))
-
-	def terminate_bot(self, botname):
+	def terminate_bot( self, botname ):
 		self.update("insert into kill_client values('%s');" % botname)
 
 	def delete_player( self, name):
 		self.update("insert into kill_client values('%s');" % name)
-		self.update("delete from players where name = '%s';" % name)
 
 	def delete_kill_name(self, name):
 		self.update("delete from kill_client where name = '%s';" % name)
-	
-	#DEPRECATED
-	def update_player_skill( self, name, skill, mu, sig ):
-		self.update_defered("update players set ngames=ngames+1,lastseen=?,skill=?,mu=?,sigma=? where name=?", (self.now(),skill,mu,sig,name))
-	
-	#DEPRECATED
-	def update_player_status(self, name, status):
-		self.update_defered("update players set status=? where name=?", (status,name))
+
+	def update_player_skill( self, t_id, bot_id, skill, mu, sig ):
+		self.update_defered("update Tourn_Entries set ngames=ngames+1, lastseen=?, skill=?, mu=?, sigma=? where tourn_id=? bot_id=?",
+			(self.now(), skill, mu, sig, t_id, bot_id))
+
+	def update_player_status(self, t_id, bot_id, status):
+		self.update_defered("update Tourn_Entries set status=? where tourn_id=? bot_id=?", (status, t_id, bot_id))
 
 	## needs a final commit() 
-	def update_player_rank( self, name, rank ):
-		self.update_defered("update players set rank=? where name=?", (rank,name))
+	def update_player_rank( self, t_id, bot_id, rank ):
+		self.update_defered("update Tourn_entries set rank=? where tourn_id=? bot_id=?", (rank, t_id, bot_id))
 		
 	#~ def get_opts( self, opts ):
 		#~ r = self.retrieve( "select * from opts" )
