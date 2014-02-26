5ade69645efa4d54cd4d66a1bde3a2e89f3e978e
Eric Liao <rcliao01@gmail.com>
2014-02-26 02:55:43 -0800

    able to init the ant object in the game template

--- gametemplate.py
+++ gametemplate.py
@@ -3,13 +3,15 @@ import json
 
 from ants import Ants
 
-operation = sys.stdin.readline()
-
-if operation == '-opts':
-    opts = sys.stdin.readline()
-    game = Game(json.loads(opts))
-
-
-class Game:
+class Game(object):
     def __init__(self, opts):
         self.ants = Ants(opts)
+        sys.stdout.write('-done\n')
+        sys.stdout.flush()
+
+if __name__ == "__main__":
+    operation = sys.stdin.readline().strip()
+
+    if operation == "-opts":
+        opts = sys.stdin.readline()
+        game = Game(json.loads(opts))
